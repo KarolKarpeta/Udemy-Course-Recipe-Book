fd5d9d37036bd3f60da0650c5f3fe4cb132d9e1d
Karol Karpeta <karol.karpeta@gmail.com>
2020-03-04 13:17:52 +0100

    transforming response data (if ingredients of a recipe do not exist, add empty array as ingredients, to avoid errors, while calling ingredients) (282.)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,24 +9,26 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-private recipes: Recipe[] = [
-    new Recipe(
-        'Schabowy',
-        'No naprawdę pyszny schabowy. Klasyka polskiej kuchni.',
-        'https://static.smaker.pl/photos/6/3/4/634320b0d13462d3176f0896942fa7f0_110705_57b32fe3190aa_wm.jpg',
-        [
-            new Ingredient('schab ze świniaka', 1),
-            new Ingredient('parnierka', 1)
-        ]),
-    new Recipe('Pierogi leniwe',
-    'Jak na Kolejowej.',
-    'https://d3iamf8ydd24h9.cloudfront.net/pictures/articles/2019/08/1065827-v-1500x1500.jpg',
-    [
-        new Ingredient('ser', 1),
-        new Ingredient('monka', 1),
-        new Ingredient('reszta', 1)
-    ]),
-];
+// private recipes: Recipe[] = [
+//     new Recipe(
+//         'Schabowy',
+//         'No naprawdę pyszny schabowy. Klasyka polskiej kuchni.',
+//         'https://static.smaker.pl/photos/6/3/4/634320b0d13462d3176f0896942fa7f0_110705_57b32fe3190aa_wm.jpg',
+//         [
+//             new Ingredient('schab ze świniaka', 1),
+//             new Ingredient('parnierka', 1)
+//         ]),
+//     new Recipe('Pierogi leniwe',
+//     'Jak na Kolejowej.',
+//     'https://d3iamf8ydd24h9.cloudfront.net/pictures/articles/2019/08/1065827-v-1500x1500.jpg',
+//     [
+//         new Ingredient('ser', 1),
+//         new Ingredient('monka', 1),
+//         new Ingredient('reszta', 1)
+//     ]),
+// ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
 
