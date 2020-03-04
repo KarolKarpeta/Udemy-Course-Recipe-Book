b15f11f2811e5bce0bcf124c791ec72f599f5c82
Karol Karpeta <karol.karpeta@gmail.com>
2020-03-04 13:11:44 +0100

    Saving and Fetching data on Firebase DB (280-281.)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -30,6 +30,11 @@ private recipes: Recipe[] = [
 
     constructor(private slService: ShoppingListService) {}
 
+    setRecipes(recipes: Recipe[]) {
+      this.recipes = recipes;
+      this.recipesChanged.next(recipes.slice());
+    }
+
     getRecipes() {
         return this.recipes.slice();
     }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import {RecipeService} from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http.put('https://ng-recipe-book-9fb10.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>('https://ng-recipe-book-9fb10.firebaseio.com/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
 }
